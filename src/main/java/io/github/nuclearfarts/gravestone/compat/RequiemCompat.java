721848af09b2a47d87296c70b7c995cd2a05c4bf
plusls <plusls@qq.com>
2020-06-17 10:09:50 -0400

    disabled requiem for snapshot testing

--- src/main/java/io/github/nuclearfarts/gravestone/compat/RequiemCompat.java
+++ src/main/java/io/github/nuclearfarts/gravestone/compat/RequiemCompat.java
@@ -3,16 +3,16 @@ package io.github.nuclearfarts.gravestone.compat;
 import java.util.function.Consumer;
 
 import io.github.nuclearfarts.gravestone.util.GravestoneUtil;
-import ladysnake.requiem.api.v1.possession.Possessable;
+// import ladysnake.requiem.api.v1.possession.Possessable;
 import net.minecraft.entity.LivingEntity;
 
 public class RequiemCompat implements Consumer<LivingEntity> {
 	
 	@Override
 	public void accept(LivingEntity entity) {
-		if(entity instanceof Possessable && ((Possessable)entity).isBeingPossessed()) {
-			GravestoneUtil.placeGrave(((Possessable)entity).getPossessor());
-		}
+		// if(entity instanceof Possessable && ((Possessable)entity).isBeingPossessed()) {
+		// 	GravestoneUtil.placeGrave(((Possessable)entity).getPossessor());
+		// }
 	}
 
 }
